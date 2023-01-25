9b82350666cdb8fed8874811db535bcc75bd015c
Claire Vanlerberghe <vanlerberghe24@gmail.com>
2023-01-25 16:28:47 +0100

    "Création du composant Banner"

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import Banner from './Banner'
-import '../App.css';
+import '../styles/App.css';
 
 function App() {
   return (
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,23 @@
 import logo from '../logo.png';
+import '../styles/Banner.css';
+import { Link } from "react-router-dom";
+
 
 function Banner() {
     return (
-    <img src={logo} className="App-logo" alt="logo" />
-    
+<nav className="nav">
+    <Link to="/">
+    <img src={logo} className="Nav-logo" alt="logo kasa" />
+    </Link>
+    <div>
+        <Link className="nav-link" to="/">
+            Accueil
+        </Link>
+        <Link className="nav-link" to="/"> 
+            A propos
+        </Link>
+    </div>
+    </nav>
     )
 }
 export default Banner
